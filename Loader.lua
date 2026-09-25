6b3e3b6ca8e090e9c208419c81be1254b9580001
hotibody99828 <lyybigber1616@gmail.com>
2026-09-26 01:17:31 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -6,7 +6,7 @@
 
 local BASE_URL = "https://raw.githubusercontent.com/hotibody99828/stealvip2/main/"
 
-_G.YOKUDO_EnablePrint = false
+_G.YOKUDO_EnablePrint = true
 
 local oldPrint = print
 print = function(...)
