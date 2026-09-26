2450b2368c52735387ad1b76e3c69f4748b2ab4c
hotibody99828 <lyybigber1616@gmail.com>
2026-09-26 13:39:14 +0700

    Create DontUseIt.lua

---
+++ Tabs/DontUseIt.lua
@@ -0,0 +1,19 @@
+-- ==================================================
+-- YOKUDO HUB | TAB | Don't Use It
+-- ==================================================
+
+local TabsManager = _G.YOKUDO_TabsManager
+local TweenService = game:GetService("TweenService")
+
+local DontUseItTab, DontUseItPage = TabsManager:RegisterTab("Don't Use It", 8, "DONT_USE_IT")
+
+-- ==================================================
+-- CONTENT
+-- ==================================================
+CreateSectionTitle(DontUseItPage, "Don't Use It", 1)
+
+-- ==================================================
+-- ទំនេរ — បន្ថែម Features នៅទីនេះ
+-- ==================================================
+
+print("✅ Don't Use It Tab Loaded")
